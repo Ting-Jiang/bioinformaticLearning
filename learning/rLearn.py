d9fe322542ff5b18e43081d3c0b68150940b8fec
Ting-Jiang <tjiang9091@gmail.com>
2020-03-07 18:12:11 +0800

    updating learning directory

---
+++ learning/rLearn.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+# Project : 
+# Author: Jiang Ting
+# Email: 
+
+print("Hello World! I would like to  learn Programming from scratch")
+
+# Flow Control
+temp=False
+
+if not temp:
+    print("tempt variable is False")
+
+
+for i in range(10):
+    print(i)
+
+
+
